0ded0761c5e3956ad5ac3421772aa312ab2a30c1
munyeong02 <59675361+munyeong02@users.noreply.github.com>
2023-03-11 13:43:26 +0900

    create project

---
+++ src/main/java/com/highboy/highboy_backend/HighboyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.highboy.highboy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighboyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighboyBackendApplication.class, args);
+	}
+
+}
