ba23ab309765a3dce2a7f972c5dcaa5345d1707d
max.cha <max.cha@kakaocorp.com>
2023-03-19 15:11:22 +0900

    init

---
+++ src/main/java/com/highboy/server/HighboyApplication.java
@@ -0,0 +1,13 @@
+package com.highboy.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighboyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighboyApplication.class, args);
+    }
+
+}
